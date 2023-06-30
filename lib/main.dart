ea7bd4ec0f76582a0cda5a452244422ba5b13475
Pradyumna2023 <127931219+Pradyumna2023@users.noreply.github.com>
2023-06-30 13:56:27 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:pgocontest/views/screen/splash_screen.dart';
+void main() async{
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primarySwatch: Colors.deepPurple
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
+
